da8767f1c612300c691dcc092929a86c8b2f0677
Vasil Hristov <vasildimitrovhristov@gmail.com>
2019-07-02 09:03:27 +0300

    Release ready

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Branch develop</Text>
+        <Text style={styles.welcome}>New release</Text>
       </View>
     );
   }
